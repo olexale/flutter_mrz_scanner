3cb6ec7c0ad1f5f05cd0088e639251e451a6e3e0
Ahmet Veysel <avs@Ahmet-Veysel-MacBook-Pro.local>
2025-06-13 10:36:06 +0300

    fix android release v1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mrz_scanner_example/camera_page.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,34 +6,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FutureBuilder<PermissionStatus>(
-        future: Permission.camera.request(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data == PermissionStatus.granted) {
-            return CameraPage();
-          }
-          if (snapshot.data == PermissionStatus.permanentlyDenied) {
-            // The user opted to never again see the permission request dialog for this
-            // app. The only way to change the permission's status now is to let the
-            // user manually enable it in the system settings.
-            openAppSettings();
-          }
-          return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CircularProgressIndicator(),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text('Awaiting for permissions'),
-                  ),
-                  Text('Current status: ${snapshot.data?.toString()}'),
-                ],
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircularProgressIndicator(),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Awaiting for permissions'),
               ),
-            ),
-          );
-        },
+              Text('Current status: toString()}'),
+            ],
+          ),
+        ),
       ),
     );
   }
